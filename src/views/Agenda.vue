322ef121c1229229ef9c878c9b7b1489db6d917e
dzakirafabillah <zigdzak27@gmail.com>
2022-05-28 20:46:25 +0700

    Update Agenda.vue (finish)

--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -15,9 +15,10 @@
                           <v-card class="mt-3 ml-3 mr-3" >
                             <v-row class="mt-1 mb-1 agenda-card">
                               <v-col cols="auto" id="tanggal">
-                                <!-- <p>{{ item.tanggal }}</p>
+                                <p>{{ item.tanggal }}</p>
                                 <p>{{ item.bulan }}</p>
-                                <p>{{ item.tahun }}</p> -->
+                                <p>{{ item.tahun }}</p>
+                                 <!-- <p>{{ item.Date }}</p> -->
                               </v-col>
 
                               <v-col cols="auto">
@@ -65,45 +66,25 @@ export default {
     Footer
   },
   data: () => ({
-  
-      agendas: [
-        // {nama_agenda: "Agenda 1",
-        //  tanggal : "10",
-        //  bulan : "APR",
-        //  tahun : "2022",
-        //  lokasi : "Politeknik Negeri Bandung",
-        //  deskripsi : " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate luctus imperdiet proin tristique sagittis, elementum. Sit platea purus elementum in eu sed volutpat. Nunc massa aliquam at quam egestas nam. Sem eget eget accumsan dictum dui. Sit commodo, non quam elementum, sit suspendisse euismod ante mi. Volutpat pellentesque tempor, lectus auctor id et. Morbi mi risus, et a. Eget nibh blandit erat magna eget sagittis." 
-        // },
-        // {nama_agenda: "Agenda 2",
-        //  tanggal : "10",
-        //  bulan : "APR",
-        //  tahun : "2022",
-        //  lokasi : "Politeknik Negeri Bandung",
-        //  deskripsi : " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate luctus imperdiet proin tristique sagittis, elementum. Sit platea purus elementum in eu sed volutpat. Nunc massa aliquam at quam egestas nam. Sem eget eget accumsan dictum dui. Sit commodo, non quam elementum, sit suspendisse euismod ante mi. Volutpat pellentesque tempor, lectus auctor id et. Morbi mi risus, et a. Eget nibh blandit erat magna eget sagittis." 
-        // },
-        // {nama_agenda: "Agenda 3",
-        //  tanggal : "10",
-        //  bulan : "APR",
-        //  tahun : "2022",
-        //  lokasi : "Politeknik Negeri Bandung",
-        //  deskripsi : " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate luctus imperdiet proin tristique sagittis, elementum. Sit platea purus elementum in eu sed volutpat. Nunc massa aliquam at quam egestas nam. Sem eget eget accumsan dictum dui. Sit commodo, non quam elementum, sit suspendisse euismod ante mi. Volutpat pellentesque tempor, lectus auctor id et. Morbi mi risus, et a. Eget nibh blandit erat magna eget sagittis." 
-        // },
-        // {nama_agenda: "Agenda 4",
-        //  tanggal : "10",
-        //  bulan : "APR",
-        //  tahun : "2022",
-        //  lokasi : "Politeknik Negeri Bandung",
-        //  deskripsi : " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci vulputate luctus imperdiet proin tristique sagittis, elementum. Sit platea purus elementum in eu sed volutpat. Nunc massa aliquam at quam egestas nam. Sem eget eget accumsan dictum dui. Sit commodo, non quam elementum, sit suspendisse euismod ante mi. Volutpat pellentesque tempor, lectus auctor id et. Morbi mi risus, et a. Eget nibh blandit erat magna eget sagittis." 
-        // },
-      ],
+      months: ['JAN', 'FEB', 'MAR', 'APR', 'MEI', 'JUN', 'JUL', 'AGS', 'SEP', 'OKT', 'NOV', "DES"],
+      agendas: [],
     }),
 
-    mounted() {
+  mounted() {
+    // console.log("MASUK");
     const url = "/agendas" ;
     http.get(url).then(response => {
       this.agendas = response.data;
+      for(let i=0; i<this.agendas.length; i++){
+        console.log("MASUK");
+        let tanggal_str = this.agendas[i].Date;
+        this.agendas[i].tanggal = tanggal_str.substring(8);
+        this.agendas[i].bulan = this.months[tanggal_str.substring(6,7)];   
+        this.agendas[i].tahun = tanggal_str.substring(0,4);   
+      }
     });
-    }
+    
+  }
 }
 </script>
 
